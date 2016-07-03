015212b32907dd4fb3240e1a85d5ea70246dc91a
Victoria Brown <victoriabrown0710@gmail.com>
2016-07-03 11:38:54 -0500

    Beginning of class PigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -1,1 +1,13 @@
+/*
+Program Name: Pig Latin
+Programmer:   Victoria Brown
+Date:         July 2016
+Purpose:      A program that takes a word entered by user and
+              translates it into pig latin.
+*/
 
+import java.util.Scanner;
+
+public class PigLatin {
+  
+}
